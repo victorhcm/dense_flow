2f0240d585d90a0eb81c03d19070ee4e1c5a1306
Victor Melo <victorhcmelo@gmail.com>
2016-07-25 20:00:57 -0300

    Add std:: and easylogging include

--- include/dense_flow.h
+++ include/dense_flow.h
@@ -23,8 +23,8 @@ void calcDenseFlowPureGPU(std::string file_name, int bound, int type, int step, 
                       std::vector<std::vector<uchar> >& output_img);
 
 void calcDenseWarpFlowGPU(std::string file_name, int bound, int type, int step, int dev_id,
-                          vector<vector<uchar> >& output_x,
-                          vector<vector<uchar> >& output_y);
+                          std::vector<std::vector<uchar> >& output_x,
+                          std::vector<std::vector<uchar> >& output_y);
 
 void MatchFromFlow_copy(const Mat& prev_grey, const Mat& flow_x, const Mat& flow_y, std::vector<Point2f>& prev_pts, std::vector<Point2f>& pts, const Mat& mask);
 
