7b5fea64bc249b84e9cc4d065b0b2b902f0910f2
Victor Melo <victorhcmelo@gmail.com>
2016-07-28 11:17:45 -0300

    Fix command-line arguments to CV 3.1

--- tools/extract_warp_flow_gpu.cpp
+++ tools/extract_warp_flow_gpu.cpp
@@ -9,14 +9,14 @@ int main(int argc, char** argv){
 	// IO operation
 	const char* keys =
 		{
-			"{ f  | vidFile      | ex2.avi | filename of video }"
-			"{ x  | xFlowFile    | flow_x | filename of flow x component }"
-			"{ y  | yFlowFile    | flow_y | filename of flow x component }"
-			"{ b  | bound | 15 | specify the maximum of optical flow}"
-			"{ t  | type | 0 | specify the optical flow algorithm }"
-			"{ d  | device_id    | 0  | set gpu id}"
-			"{ s  | step  | 1 | specify the step for frame sampling}"
-			"{ o  | out | zip | output style}"
+			"{ f vidFile   | ex2.avi | filename of video }"
+			"{ x xFlowFile | flow_x  | filename of flow x component }"
+			"{ y yFlowFile | flow_y  | filename of flow y component }"
+			"{ b bound     | 15      | specify the maximum of optical flow}"
+			"{ t type      | 0       | specify the optical flow algorithm }"
+			"{ d device_id | 0       | set gpu id}"
+			"{ s step      | 1       | specify the step for frame sampling}"
+			"{ o out       | zip     | output style}"
 		};
 
 	CommandLineParser cmd(argc, argv, keys);
